de096459ba486964f7c90c6aa7c6bd0cfb6a87b2
Aymeric Wibo <obiwac@gmail.com>
2025-01-16 01:06:15 +0100

    clang-format: Regression test for indenting initializers

--- src/c/hello_world.c
+++ src/c/hello_world.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+	struct {
+		int a;
+		int b;
+	} nested;
+} my_struct_t;
+
 int main(void) {
 	printf("Hello world!\n");
+
+	my_struct_t const my_struct = {
+		.nested = {
+			.a = 1,
+			.b = 2,
+		},
+	};
+
 	return EXIT_SUCCESS;
 }
